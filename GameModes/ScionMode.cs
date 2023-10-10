c4f9d2107097776d3a164aff3ffa866acec8eac3
Alexei Korol <korolalexei@gmail.com>
2023-10-10 19:32:44 +0400

    aider: Added C:\Users\16178\source\repos\EU\Utils\Layout.cs

--- GameModes/ScionMode.cs
+++ GameModes/ScionMode.cs
@@ -1,6 +1,6 @@
 namespace EpochsUnbound.GameModes
 {
-    public class ScionChampionMode
+    public class ScionMode
     {
         // Add methods and properties specific to the Scion/Champion mode here
     }
