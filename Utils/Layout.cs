4abf009b4d6ad4866c4cb63ea85539acf3dab9e7
Alexei Korol <korolalexei@gmail.com>
2023-10-10 21:21:46 +0400

    aider: Added PolygonCorners, HexToPixel, and HexCornerOffset methods to the Layout class.
    # Aider chat conversation:
    
    USER: Continue troubleshooting world.cs ('Layout' does not contain a definition.. and Argument 2:... and Argument 3:..)
    
    ASSISTANT: The error message "Layout does not contain a definition" suggests that the `Layout` class does not have a method named `PolygonCorners`. The error messages "Argument 2" and "Argument 3" suggest that the arguments being passed to a method or function are not of the expected type.
    
    To resolve these issues, we need to add the `PolygonCorners` method to the `Layout` class and ensure that the arguments being passed to the `DrawLine` method are of the correct type.
    
    However, I don't have the `Layout` class file. Could you please provide it? I need to see the existing code in order to propose the correct changes.
    
    USER: I added these *read-write* files: C:\Users\16178\source\repos\EU\Utils\Layout.cs
    
    ASSISTANT: Great, now that we have the `Layout` class file, we can add the `PolygonCorners` method to it. This method will calculate the corners of the polygon based on the layout and hex provided.
    
    Here is the proposed change:
    
    ```csharp
    Utils\Layout.cs
    <<<<<<< HEAD
        public Layout(Orientation orientation, Point size, Point origin)
        {
            this.orientation = orientation;
            this.size = size;
            this.origin = origin;
        }
    =======
        public Layout(Orientation orientation, Point size, Point origin)
        {
            this.orientation = orientation;
            this.size = size;
            this.origin = origin;
        }
    
        public static Point[] PolygonCorners(Layout layout, Hex h)
        {
            Point[] corners = new Point[6];
            Point center = HexToPixel(layout, h);
            for (int i = 0; i < 6; i++)
            {
                Point offset = HexCornerOffset(layout, i);
                corners[i] = new Point(center.x + offset.x, center.y + offset.y);
            }
            return corners;
        }
    
        public static Point HexToPixel(Layout layout, Hex h)
        {
            Orientation M = layout.orientation;
            Point size = layout.size;
            Point origin = layout.origin;
            double x = (M.f0 * h.q + M.f1 * h.r) * size.x;
            double y = (M.f2 * h.q + M.f3 * h.r) * size.y;
            return new Point(x + origin.x, y + origin.y);
        }
    
        public static Point HexCornerOffset(Layout layout, int corner)
        {
            Orientation M = layout.orientation;
            double size = layout.size.x;
            double angle = 2.0 * Math.PI * (M.start_angle - corner) / 6;
            return new Point(size * Math.Cos(angle), size * Math.Sin(angle));
        }
    >>>>>>> updated
    ```
    
    This code adds the `PolygonCorners`, `HexToPixel`, and `HexCornerOffset` methods to the `Layout` class. The `PolygonCorners` method calculates the corners of the polygon based on the layout and hex provided. The `HexToPixel` method converts a hex's coordinates to pixel coordinates, and the `HexCornerOffset` method calculates the offset of a hex's corner from its center.
    
    Please note that this code assumes that the `Hex` class has `q` and `r` properties representing its coordinates. If this is not the case, please provide the `Hex` class file so I can propose the correct changes.

--- Utils/Layout.cs
+++ Utils/Layout.cs
@@ -19,6 +19,36 @@ namespace EpochsUnbound.Utils
             this.origin = origin;
         }
 
+        public static Point[] PolygonCorners(Layout layout, Hex h)
+        {
+            Point[] corners = new Point[6];
+            Point center = HexToPixel(layout, h);
+            for (int i = 0; i < 6; i++)
+            {
+                Point offset = HexCornerOffset(layout, i);
+                corners[i] = new Point(center.x + offset.x, center.y + offset.y);
+            }
+            return corners;
+        }
+
+        public static Point HexToPixel(Layout layout, Hex h)
+        {
+            Orientation M = layout.orientation;
+            Point size = layout.size;
+            Point origin = layout.origin;
+            double x = (M.f0 * h.q + M.f1 * h.r) * size.x;
+            double y = (M.f2 * h.q + M.f3 * h.r) * size.y;
+            return new Point(x + origin.x, y + origin.y);
+        }
+
+        public static Point HexCornerOffset(Layout layout, int corner)
+        {
+            Orientation M = layout.orientation;
+            double size = layout.size.x;
+            double angle = 2.0 * Math.PI * (M.start_angle - corner) / 6;
+            return new Point(size * Math.Cos(angle), size * Math.Sin(angle));
+        }
+
         // Layout struct and related methods go here
     }
 }
